e7c0df527c5259fab4f45bce07fa3b154d1ba551
foo <odk.regis@gmail.com>
2016-05-27 02:50:23 +0000

    implement pictureUploader by paperclip

---
+++ app/models/picture.rb
@@ -0,0 +1,13 @@
+class Picture < ActiveRecord::Base
+   # photoをattachファイルとする。stylesで画像サイズを定義できる
+  has_attached_file :photo, 
+  :styles => { medium: "300x300>", thumb: "100x100>" },
+  :url  => "/assets/arts/:id/:style/:basename.:extension", # 画像保存先のURL先
+	:path => "#{Rails.root}/public/assets/arts/:id/:style/:basename.:extension" # サーバ上の画像保存先パス
+  
+  validates_attachment :photo, 
+  presence: true,  # ファイルの存在チェック
+      less_than: 5.megabytes, # ファイルサイズのチェック
+  content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+                  # ファイルの拡張子を指定（これがないとエラーが発生する）
+end
