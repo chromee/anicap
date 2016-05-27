42962ef1886ed17fd9c17dec99f9ab8184b34f71
foo <odk.regis@gmail.com>
2016-05-27 08:29:49 +0000

    implement tags

--- Gemfile
+++ Gemfile
@@ -9,7 +9,9 @@ gem 'jquery-rails', '4.0.3'
 gem 'turbolinks',   '2.3.0'
 gem 'jbuilder',     '2.2.3'
 gem 'sdoc',         '0.4.0', group: :doc
+
 gem "paperclip"
+gem 'acts-as-taggable-on', '~> 3.4'
 
 group :development, :test do
   gem 'sqlite3',     '1.3.9'
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -10,4 +10,6 @@ class Picture < ActiveRecord::Base
       less_than: 5.megabytes, # ファイルサイズのチェック
   content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
                   # ファイルの拡張子を指定（これがないとエラーが発生する）
+                  
+  acts_as_ordered_taggable_on :tags
 end
